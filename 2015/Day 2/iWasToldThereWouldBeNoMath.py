b8330d4c4685b657b22af3d3db21f182a4e282b0
rokija <rokijagames@gmail.com>
2024-07-18 16:19:00 +0200

    done

--- 2015/Day 2/iWasToldThereWouldBeNoMath.py
+++ 2015/Day 2/iWasToldThereWouldBeNoMath.py
@@ -1,8 +1,12 @@
 import numpy as np 
 
 file = np.genfromtxt('input.txt', dtype=str)
+sum = 0
 
-file1 = file.splitlines()
-
-print(file1)
-            
+for g in range(len(file)):
+    x, y, z = file[g].split('x')
+    l, w, h = int(x), int(y), int(z)
+    slack = min(l*w, w*h, h*l)
+    surfaceArea = 2*(l*w) + 2*(w*h) + 2*(h*l) + slack
+    sum += surfaceArea
+print(sum)
