77399e6a6f16076a0920f69e90aa15d90a711ee8
rokija <rokijagames@gmail.com>
2024-07-18 14:58:09 +0200

    removed old stuff, now ill have to splitlines txt

--- 2015/Day 2/iWasToldThereWouldBeNoMath.py
+++ 2015/Day 2/iWasToldThereWouldBeNoMath.py
@@ -1,11 +1,8 @@
 import numpy as np 
 
-file = np.loadtxt('input.txt', dtype=str)
+file = np.genfromtxt('input.txt', dtype=str)
 
-for x in range(0,len(file)):
-    to_array = list(file[x])
-    for y in range(0,len(to_array)):
-        if to_array[y] != 'x':
-            print(to_array[y])
+file1 = file.splitlines()
 
+print(file1)
             
