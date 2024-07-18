b2f10ee5b243a0e77c6475e1951ccef7af488580
rokija <rokijagames@gmail.com>
2024-07-18 16:27:34 +0200

    done

--- 2015/Day 2 - I Was Told There Would Be No Math/iWasToldThereWouldBeNoMath.py
+++ 2015/Day 2 - I Was Told There Would Be No Math/iWasToldThereWouldBeNoMath.py
@@ -4,8 +4,8 @@ file = np.genfromtxt('input.txt', dtype=str)
 sum = 0
 
 for g in range(len(file)):
-    x, y, z = file[g].split('x')
-    l, w, h = int(x), int(y), int(z)
+    l, w, h = file[g].split('x')
+    l, w, h = int(l), int(w), int(h)
     slack = min(l*w, w*h, h*l)
     surfaceArea = 2*(l*w) + 2*(w*h) + 2*(h*l) + slack
     sum += surfaceArea
